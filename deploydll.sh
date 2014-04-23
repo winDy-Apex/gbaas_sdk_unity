4f0b8124d74b1c9d0ed44f1124404627ed162ea5
winDy <winDy@apexplatform.net>
2014-04-23 15:51:24 +0900

    Change Deploydll.sh

--- deploydll.sh
+++ deploydll.sh
@@ -3,11 +3,13 @@ cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/G
 cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/bin/Debug/* /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/Test\ Photon\ Project/Assets/Plugins/
 rm -rf /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
 mkdir /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
+
 cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/bin/Debug/* /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
-cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/GBaaSApi.cs /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
-cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/GBaaSApiHandler.cs /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
-cp -r /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/Objects /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
-cp -r /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/Services /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
-cp -r /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/Utils /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
-rm /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/GBaaS.io.dll
-rm /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/GBaaS.io.dll.mdb
+
+#cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/GBaaSApi.cs /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
+#cp /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/GBaaSApiHandler.cs /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
+#cp -r /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/Objects /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
+#cp -r /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/Services /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
+#cp -r /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/gbaas_sdk_unity/GBaaS.io/Utils /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/
+#rm /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/GBaaS.io.dll
+#rm /Users/winDy/Documents/windystudio_svn/PROJECT/Apexplatform/unity2d_testgame/Assets/Standard\ Assets/GBaaS/GBaaS.io.dll.mdb
